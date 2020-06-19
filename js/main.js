99a59d8a2ebc7127a0a17d2b11cb6d9ebf3a2d8a
yuon flemming <yuonflemming@aol.com>
2020-06-19 00:28:03 -0400

    Updated various web pages/added pics/carousel template

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     $("#banner-placeholder").load("templates/banner.html");
 });
 
+$(document).ready(function(){
+  $("#projects-carousel").load("templates/projects_carousel.html");
+});
+
 // When a user clicks on a button, all <p> elements will be hidden:
 
 // Example
