1b32022f292108f4f5b0dce4c1d435ffe8204297
Yuon Flemming <yuonflemming@aol.com>
2024-01-30 18:58:02 -0500

    Updates to look of landing page
    added About section to the bottom
    changed home page pics, and positioning

--- js/main.js
+++ js/main.js
@@ -17,6 +17,20 @@ function setBanner() {
   }
 }
 
+function infiniteScroll() {
+  var page=1;
+  window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      ifrm = document.createElement("IFRAME"); 
+      ifrm.setAttribute("resume_page", page+".html"); 
+      ifrm.style.width = 100+"%"; 
+      ifrm.style.height = 800+"px"; 
+      document.body.appendChild(ifrm); 
+      page++
+    }
+  };
+}
+
 // $(document).ready(function(){
 //   $("#footer").load("");
 // });
